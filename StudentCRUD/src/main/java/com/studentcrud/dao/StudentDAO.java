77329071187035f13a80c6e2773c9ded1bd56451
NareshLokhande <nareshlokhande49@gmail.com>
2024-06-06 09:58:51 +0530

    StudentCRUD Project Started

---
+++ StudentCRUD/src/main/java/com/studentcrud/dao/StudentDAO.java
@@ -0,0 +1,14 @@
+package com.studentcrud.dao;
+
+import java.util.List;
+
+import com.studentcrud.models.Student;
+
+public interface StudentDAO {
+
+	int save(Student student);
+	List<Student> getAll();
+	Student getById(int id);
+	int remove(int id);
+	
+}
---
+++ StudentCRUD/src/main/java/com/studentcrud/daoimpl/StudentDAOImpl.java
@@ -0,0 +1,36 @@
+package com.studentcrud.daoimpl;
+
+import java.util.List;
+
+import com.studentcrud.dao.StudentDAO;
+import com.studentcrud.models.Student;
+
+public class StudentDAOImpl implements StudentDAO{
+
+	@Override
+	public int save(Student student) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Student> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student getById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public int remove(int id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
