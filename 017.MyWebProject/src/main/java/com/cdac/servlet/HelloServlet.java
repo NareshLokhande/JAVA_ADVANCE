5165bce91eb7656c120ef5ba30079ca69423ba8b
NareshLokhande <nareshlokhande49@gmail.com>
2024-06-17 04:01:52 +0530

    Updated

--- 017.MyWebProject/src/main/java/com/cdac/servlet/HelloServlet.java
+++ 017.MyWebProject/src/main/java/com/cdac/servlet/HelloServlet.java
@@ -4,20 +4,16 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDate;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloServlet
- */
-@WebServlet("/HelloServlet")
+@WebServlet("/hello.cdac")	//URL pattern or servlet mapping
 public class HelloServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-
+@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			PrintWriter out = response.getWriter();
 			out.write("<html><body>");
